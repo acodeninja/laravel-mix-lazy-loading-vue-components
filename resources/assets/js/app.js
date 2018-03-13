75b15fcabf9b9a7b84fb8c3f43f358e624a2e194
Lawrence <lawrence@acode.ninja>
2018-03-13 12:12:44 +0000

    Lazy load the example vue component

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -15,7 +15,10 @@ window.Vue = require('vue');
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 
-Vue.component('example-component', require('./components/ExampleComponent.vue'));
+Vue.component(
+    'example-component',
+    () => import(/* webpackChunkName: "example-component" */ './components/ExampleComponent.vue')
+);
 
 const app = new Vue({
     el: '#app'
