52116781379d3f829909cc21bd8bd3f8ce1e2676
Lawrence <lawrence@acode.ninja>
2018-03-13 12:12:08 +0000

    Override mix webpack output configuration

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,14 @@ let mix = require('laravel-mix');
  |
  */
 
+/**
+ * Override Laravel Mix Webpack Configuration
+ * @type {{chunkFilename: string, publicPath: string}}
+ */
+mix.config.webpackConfig.output = {
+    chunkFilename: 'js/[name].bundle.js',
+    publicPath: '/',
+};
+
 mix.js('resources/assets/js/app.js', 'public/js')
    .sass('resources/assets/sass/app.scss', 'public/css');
